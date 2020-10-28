ed352235dd574d5c12268aa0af205e649d289571
Turyansu <tsubhadarshy@gmail.com>
2020-10-28 20:49:00 +0530

    Activity2_2

---
+++ Java/Activity_2_2/Plane.java
@@ -0,0 +1,35 @@
+package JavaActivity2_2;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+class Plane {
+    private List<String> passengers;
+    private int maxPassengers;
+    private Date lastTimeTookOf;
+    private Date lastTimeLanded;
+    
+    public Plane(int maxPassengers) {
+    	this.maxPassengers = maxPassengers;
+    	this.passengers = new ArrayList();
+    }
+    public void onboard(String passenger) {
+    	this.passengers.add(passenger);
+    }
+    public Date takeOff() {
+        this.lastTimeTookOf = new Date();
+        return lastTimeTookOf;
+    }
+    public void land() {
+        this.lastTimeLanded = new Date();
+        this.passengers.clear();
+    }
+    public Date getLastTimeLanded() {
+        return lastTimeLanded;
+    }
+
+    public List<String> getPassengers() {
+        return passengers;
+    }
+}
